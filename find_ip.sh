6a432f65e2975be49d02d9991471d62fe3ff90c9
ramnamdas <ram.nettech@gmail.com>
2018-08-25 17:44:10 +0530

    Update find_ip.sh

--- find_ip.sh
+++ find_ip.sh
@@ -4,4 +4,4 @@ aws ec2 describe-instances --filters "Name=tag:Name,Values=elastic" | grep "Publ
 echo "[kibana]" >> /var/lib/jenkins/myip.txt
 aws ec2 describe-instances --filters "Name=tag:Name,Values=kibana" | grep "PublicIpAddress"| cut -d '"' -f4 >> /var/lib/jenkins/myip.txt
 echo "[logstash]" >> /var/lib/jenkins/myip.txt
-aws ec2 describe-instances --filters "Name=tag:Name,Values=kibana" | grep "PublicIpAddress" | cut -d '"' -f4>> /var/lib/jenkins/myip.txt
+aws ec2 describe-instances --filters "Name=tag:Name,Values=logstash" | grep "PublicIpAddress" | cut -d '"' -f4>> /var/lib/jenkins/myip.txt
