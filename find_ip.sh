0a189b14857086699da851d8d6c979c245e50a7f
ramnamdas <ram.nettech@gmail.com>
2018-08-27 22:16:00 +0530

    Update find_ip.sh

--- find_ip.sh
+++ find_ip.sh
@@ -14,5 +14,5 @@ aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "
 while [ -z $elasticsearch_ip ]
 do
 elasticsearch_ip=`aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" "Name=tag:Name,Values=elastic" | grep -i "privateipaddress" | head -n1 | cut -d '"' -f4`
-echo $elasticsearch_ip > /var/lib/jenkins/elastic.txt
 done
+echo $elasticsearch_ip > /var/lib/jenkins/elastic.txt
